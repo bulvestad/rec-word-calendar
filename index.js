f9be10d22ca10938a1f19b5cf07ee71768a70fa7
fibeulve <bente.ulvestad@finn.no>
2019-12-25 17:16:31 +0100

    added walking skeleton webapp

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 const express = require('express');
 const Podlet = require('@podium/podlet');
+
+const app = express();
+
+const podlet = new Podlet({
+  name: 'rec-word-clock', // required
+  version: '1.0.0', // required
+  pathname: '/', // required
+  development: true,
+});
+
+app.use(podlet.middleware());
+
+app.get(podlet.manifest(), (req, res) => {
+  res.status(200).send(podlet);
+});
+
+app.get(podlet.content(), (req, res) => {
+
+  res.podiumSend(
+      `<div>
+        <h1>Title</h1>
+        <p>Hola sweet Julian! </p>
+    </div>`
+  );
+});
+
+app.listen(7100);
